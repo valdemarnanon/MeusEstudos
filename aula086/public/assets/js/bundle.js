1d2a7db20390f668b5f7c93af353112b2bd76f8d
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2024-10-15 19:10:49 -0300

    JS

---
+++ aula086/public/assets/js/bundle.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert(1);
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
