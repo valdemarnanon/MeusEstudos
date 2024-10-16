8aa15088e0d49bee06fba899e4188f0fee612e20
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2024-10-16 18:16:53 -0300

    Modulo import / export

---
+++ aula087/src/index.js
@@ -0,0 +1,9 @@
+import { nome, sobrenome, idade, soma, Pessoa } from './modulo1';
+
+const p1 = new Pessoa(nome, sobrenome);
+console.log(p1)
+
+// import { nome2, sobrenome, idade, soma as gordaoXT } from './modulo1';
+
+// console.log(nome2, sobrenome, idade);
+// console.log(gordaoXT(20, 30));
---
+++ aula087/src/modulo1.js
@@ -0,0 +1,25 @@
+export const nome = 'Valdemar';
+export const sobrenome = 'Nanon';
+export const idade = 21;
+
+export function soma(x, y) {
+    return x + y;
+}
+
+export class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
+
+// const nome = 'Valdemar';
+// const sobrenome = 'Nanon';
+// const idade = 21;
+
+// function soma(x, y) {
+//     return x + y;
+// }
+
+// export { nome as nome2, sobrenome, idade, soma };
+
